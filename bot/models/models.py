e6c43cc08a7746e44382b6e4d51011a38b81b0d2
Ihor Orlenko <eaglyig@gmail.com>
2024-10-03 23:58:42 +0300

    feat: add validations and weekday

--- bot/models/models.py
+++ bot/models/models.py
@@ -1,7 +1,10 @@
-from pydantic import BaseModel
-from typing import List, Optional, Set, Literal
 import datetime
+import yaml
+from pydantic import BaseModel, Field, field_validator
+from typing import List, Optional, Set, Literal
 from datetime import time, date
+
+Weekday = Literal['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
 class Teacher(BaseModel):
     name: str
@@ -19,7 +22,12 @@ class Classroom(BaseModel):
     room_number: Optional[str]
 
 class Subgroup(BaseModel):
-    number: int
+    number: int = Field(
+        ..., 
+        ge=1, 
+        le=4, 
+        description="Subgroup should be in range 1...4"
+    )
 
 class ClassType(BaseModel):
     class_type: Literal['lecture', 'practice']
@@ -28,15 +36,34 @@ class UniversityClass(BaseModel):
     subject: Subject
     class_time: ClassTime
     classroom: Classroom
-    subgroups: Set[Subgroup]
+    subgroups: List[Subgroup]
     class_type: ClassType
     date: Optional[datetime.date] = None
 
 class DailySchedule(BaseModel):
-    date: date
+    day: Weekday
     classes: List[UniversityClass]
 
 class WeeklySchedule(BaseModel):
-    week_start: date
-    week_end: date
     daily_schedules: List[DailySchedule]
+
+    @field_validator('daily_schedules')
+    def validate_weekdays(cls, v):
+        weekdays = {'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'}
+        schedule_days = {schedule.day for schedule in v}
+        if schedule_days != weekdays:
+            raise ValueError('Schedule must include all days from Monday to Friday')
+        return v
+
+
+def load_schedule_from_yaml(file_path: str) -> WeeklySchedule:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        yaml_data = yaml.safe_load(file)
+    
+    weekly_schedule = WeeklySchedule.parse_obj(yaml_data)
+    
+    return weekly_schedule
+
+if __name__ == "__main__":
+    schedule = load_schedule_from_yaml('bot/resources/schedule.yaml')
+    print(schedule)
