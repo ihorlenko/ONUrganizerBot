c0e3854bf8089c62747dc7d60e87ccb5b58404af
maelys-p <hordiilisnytskyi@stud.onu.edu.ua>
2024-10-05 20:53:59 +0300

    Made an upsettingly overengineered workaround for the cat functionality introduced previously.

--- bot/app/handlers/schedule_handlers.py
+++ bot/app/handlers/schedule_handlers.py
@@ -1,3 +1,4 @@
+import random
 
 from dotenv import load_dotenv
 
@@ -19,20 +20,21 @@ from aiogram.utils.text_decorations import markdown_decoration
 
 from app.keyboards import group
 
-
 last_day = ""
 
 schedule_path = "./bot/resources/schedule.yaml"
 schedule_router: Router = Router()
 weekly_schedule: WeeklySchedule = load_schedule_from_yaml(schedule_path)
 
-
 load_dotenv()
+
 
 def escape_md(*content, sep=" "):
     def _join(*content, sep=" "):
         return sep.join(map(str, content))
+
     return markdown_decoration.quote(_join(*content, sep=sep))
+
 
 bells = [
     "08:00",
@@ -45,11 +47,12 @@ bells = [
 
 last_messages = {}
 
+
 def format_schedule_for_day(daily_schedule: DailySchedule) -> str:
     result = []
     result.append(f"*{escape_md(daily_schedule.day.upper())}:*\n")
     classes = daily_schedule.classes
-    
+
     for university_class in sorted(classes, key=lambda c: c.class_time.start_time):
         subject = escape_md(university_class.subject.name)
         teacher = escape_md(university_class.subject.teacher.name)
@@ -63,24 +66,24 @@ def format_schedule_for_day(daily_schedule: DailySchedule) -> str:
         classroom = f"{escape_md(classroom)} аудиторії" if classroom != "online" else "online"
         class_type = "Лекція" if class_type == "lecture" else "Практика"
         class_type += " в" if classroom != "online" else ""
-        
+
         teacher_name = escape_md(university_class.subject.teacher.name)
         if university_class.subject.teacher.link:
             teacher = f"[{teacher_name}]({university_class.subject.teacher.link})"
         else:
             teacher = teacher_name
-        
+
         subgroups_emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣"]
         subgroups = list(map(lambda s: subgroups_emojis[s.number - 1], university_class.subgroups))
         subgroups = ', '.join(subgroups)
         subgroups = escape_md(subgroups)
-        
+
         result.append(
             f"*{class_order} пара:* {start_time}\-{end_time}\n"
             f"*{subject}* \- {teacher}\n"
             f"{class_type} {classroom}\n"
             f"*Підгрупи:* {subgroups}")
-        
+
         result.append("")
 
     return "\n".join(result)
@@ -92,22 +95,62 @@ async def welcome_msg(message: Message):
         f"Привіт!👋 Я - бот 122 спеціальності\nНа який день показати розклад?", reply_markup=kb.main
     )
 
-@schedule_router.message(lambda message: message.text.lower() in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'])
+
+# utility handler
+@schedule_router.message(F.photo)
+async def photo_id(message: Message):
+    await message.reply(f'ID: {message.photo[-1].file_id}')
+
+
+cat_images = ["AgACAgIAAxkBAAIFlGcBfKya8Jo5haHpBhYU-KoCEERgAAIa6DEb_QQISGnc7OHhKdQDAQADAgADeQADNgQ"
+    , "AgACAgIAAxkBAAIFb2cBe-a1Xs9OuiBY6hXwe63_uhGyAAIR6DEb_QQISGF12IuC7QldAQADAgADeQADNgQ"
+              ]
+
+
+@schedule_router.message(
+    lambda message: message.text.lower() in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'])
 async def send_or_edit_schedule(message: types.Message, state: FSMContext):
+    cat_image = random.choice(cat_images)
     global last_day
     if message.text.lower() == last_day:
         await bot.delete_message(message.chat.id, message.message_id)
         return
-    last_day = message.text.lower()
     await bot.delete_message(message.chat.id, message.message_id)
     day_of_week = message.text.capitalize()
-    daily_schedule = next((schedule for schedule in weekly_schedule.daily_schedules if schedule.day == day_of_week), None)
+
+    if day_of_week == "Saturday":
+        if message.from_user.id in last_messages:
+            try:
+                await bot.delete_message(
+                    chat_id=message.chat.id,
+                    message_id=last_messages[message.from_user.id]
+                )
+            except Exception as e:
+                print(f"Error deleting message: {e}")
+
+        msg = await bot.send_photo(
+            chat_id=message.chat.id,
+            photo=cat_image
+        )
+        last_day = message.text.lower()
+        last_messages[message.from_user.id] = msg.message_id
+        return
+
+    daily_schedule = next((schedule for schedule in weekly_schedule.daily_schedules if schedule.day == day_of_week),
+                          None)
 
     if daily_schedule:
         schedule_text = format_schedule_for_day(daily_schedule)
 
         if message.from_user.id in last_messages:
-
+            if last_day.lower() == 'saturday':
+                try:
+                    await bot.delete_message(
+                        chat_id=message.chat.id,
+                        message_id=last_messages[message.from_user.id]
+                    )
+                except Exception as e:
+                    print(f"Error deleting message: {e}")
             try:
                 await bot.edit_message_text(
                     text=schedule_text,
@@ -118,14 +161,11 @@ async def send_or_edit_schedule(message: types.Message, state: FSMContext):
                     reply_markup=group
                 )
             except Exception as e:
-                #print(f"Error editing message: {e}")
                 msg = await message.answer(
                     schedule_text,
                     parse_mode=ParseMode.MARKDOWN_V2,
                     disable_web_page_preview=True,
                     reply_markup=group
-
-
                 )
                 last_messages[message.from_user.id] = msg.message_id
         else:
@@ -133,14 +173,9 @@ async def send_or_edit_schedule(message: types.Message, state: FSMContext):
                 schedule_text,
                 parse_mode=ParseMode.MARKDOWN_V2,
                 disable_web_page_preview=True,
-                    reply_markup=group
+                reply_markup=group
             )
             last_messages[message.from_user.id] = msg.message_id
+        last_day = message.text.lower()
     else:
         await message.reply(f"Розклад на {day_of_week} не знайдено.", parse_mode=ParseMode.MARKDOWN_V2)
-
-
-cat_image = "AgACAgIAAxkBAAIE1mcBcvxkV4LTuIX9NAcq7f-qlEnqAAKQ6TEbjy4JSBVVh2TtcYx1AQADAgADeQADNgQ"
-@schedule_router.message(F.text.lower() == 'saturday')
-async def cat_reply(message: types.Message):
-    await message.reply_photo(cat_image)
